38c033c657ddd719ff599b0baa10ee0dc5eb7086
devrosses <devrosses@gmail.com>
2025-07-17 05:45:38 -0300

    fix(layout): Corrige estructura de App.jsx para implementar sticky footer

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -5,11 +5,7 @@ const Layout = ({ children }) => {
   return (
     <div className="d-flex flex-column min-vh-100">
       <Navbar />
-      <main
-        className="d-flex flex-column flex-grow-1"
-      >
-        {children}
-      </main>
+      <main className="flex-grow-1">{children}</main>
       <Footer />
     </div>
   );
