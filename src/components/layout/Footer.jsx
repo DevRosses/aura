e158cb2c0950553d1cd4c632820b50be8b84b3cc
devrosses <devrosses@gmail.com>
2025-07-17 03:53:18 -0300

    feat: Añade el Footer y mejora la grilla de productos

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,7 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { ROUTES } from "../../constants/routes";
+import { Icon } from "@iconify/react";
+import "../../assets/styles/components/layout/Footer.css";
+
 function Footer() {
   return (
-    <footer className="footer pt-2 pb-5">
-      <p className="text-center text-muted">&copy; 2025 Rosario Ramos. Todos los derechos reservados.</p>
+    <footer className="footer">
+      <div className="container">
+        <div className="footer__navigation">
+          <Link to={ROUTES.ABOUT_US}>Nosotros</Link>
+          <Link to={ROUTES.CONTACT_US}>Contacto</Link>
+          <Link to={ROUTES.PRODUCTS}>Productos</Link>
+          {/* Añade más enlaces si es necesario */}
+        </div>
+        <div className="footer__social">
+          {/* Puedes añadir aquí iconos de redes sociales con enlaces */}
+          {/* <Link to="#"><Icon icon="ri:instagram-line" /></Link> */}
+          {/* <Link to="#"><Icon icon="ri:facebook-line" /></Link> */}
+        </div>
+        <div className="footer__copyright">
+          <p>
+            &copy; {new Date().getFullYear()} Aura. Todos los derechos
+            reservados.
+          </p>
+          <p className="footer__disclaimer">
+            Cosmética natural ancestral con energía y propósito.
+          </p>
+        </div>
+      </div>
     </footer>
   );
 }
