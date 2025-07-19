610fedb54af6ad8b3aef59e85bcbd0f92b3eb2dc
devrosses <devrosses@gmail.com>
2025-07-18 22:03:30 -0300

    fix(firebase): Corrige nombres de variables de entorno

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -8,6 +8,7 @@ import {
   sendEmailVerification,
 } from "firebase/auth";
 
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -16,6 +17,8 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+
+
 
 const app = initializeApp(firebaseConfig);
 
